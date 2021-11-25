97462080d783ab492eff7dfd598575eb96d176d3
Leandro Junior <leandrojr70@gmail.com>
2021-11-25 10:40:10 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            */
+
+            string[] nome = new string[5];
+
+            nome[0] = "Rosa";
+            nome[1] = "Ricardo";
+            nome[2] = "Marcos";
+            nome[3] = "João";
+            nome[4] = "Maria";
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+                Console.WriteLine(nome[i]);
+            }
+        }
+    }
+}
